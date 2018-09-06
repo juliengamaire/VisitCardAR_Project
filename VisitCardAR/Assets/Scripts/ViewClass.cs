306eeb2744a098debd1b1257dd2bfca206b897a3
Lauriane <lauriane.moy@gmail.com>
2018-09-06 16:59:44 +0200

    Lauriane : Scripts
    Enlever tous les scripts attachés aux gameobject

---
+++ VisitCardAR/Assets/Scripts/ViewClass.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewClass : MonoBehaviour {
+	public GameObject myMap;
+	public GameObject myButtonMap;
+	public GameObject myVideo;
+	public GameObject myButtonVideo;
+	public GameObject myPointer;
+	public GameObject myPortrait;
+
+	public GameObject myImageTarget;
+	public GameObject myBalloon1;
+	public GameObject myBalloon2;
+	public GameObject myBalloon3;
+
+	public Text myButtonText;
+
+	public Animator myLogoAnim;
+    public Animator myBalloon1Anim;
+    public Animator myBalloon2Anim;
+    public Animator myBalloon3Anim;
+
+}
